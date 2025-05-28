4317a1096568e9a29005f213c0b6a14664d51a75
Muhammad Rizki Nur Ma'arif <muhammadrizkinur37@gmail.com>
2025-05-28 20:42:25 +0700

    Refactor HomeController to include title data in view rendering

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,6 +9,26 @@ class HomeController extends Controller
     public function index()
     {
         // This method will return the home view
-        return view('home');
+        $data['title'] = 'Insta App';
+        return view('landing', $data);
+    }
+
+    public function home()
+    {
+        $data['title'] = 'Insta App';
+        return view('home', $data);
+    }
+
+    public function add()
+    {
+        $data['title'] = 'Add Post';
+        return view('add-post', $data);
+    }
+
+    public function detail($id)
+    {
+        $data['title'] = 'Detail Post';
+        $data['id'] = $id;
+        return view('detail-post', $data);
     }
 }
