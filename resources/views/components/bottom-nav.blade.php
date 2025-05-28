708ee3c502bc722f35c80d5c33374be0c5197f85
Muhammad Rizki Nur Ma'arif <muhammadrizkinur37@gmail.com>
2025-05-28 16:26:08 +0700

    Add bottom navigation component for mobile layout

---
+++ resources/views/components/bottom-nav.blade.php
@@ -0,0 +1,12 @@
+<!-- Bottom Navigation (Mobile) -->
+<div class="md:hidden fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 flex justify-around items-center h-16 z-10">
+    <a href="/home" class="text-gray-700 hover:text-gray-900">
+        <i class="fas fa-home text-2xl"></i>
+    </a>
+    <a href="/add" class="text-gray-700 hover:text-gray-900">
+        <i class="far fa-plus-square text-2xl"></i>
+    </a>
+    <a href="#" class="text-gray-700 hover:text-gray-900">
+        <img class="h-6 w-6 rounded-full" src="https://randomuser.me/api/portraits/women/44.jpg" alt="Profile">
+    </a>
+</div>
