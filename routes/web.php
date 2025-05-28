524cacc8a9211ba2e52312722324843318ae1569
Muhammad Rizki Nur Ma'arif <muhammadrizkinur37@gmail.com>
2025-05-28 20:42:38 +0700

    Fix logout route method from POST to GET in web.php

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,14 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
 Route::post('/register', [AuthController::class, 'register']);
 
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::middleware(['auth'])->group(function () {
    Route::get('/home', [HomeController::class, 'home'])->name('home');
    Route::get('/add', [HomeController::class, 'add'])->name('add');
    Route::get('/detail/{post}', [HomeController::class, 'detail'])->name('detail');
    Route::get('/api/posts', [ApiController::class, 'getPosts']);
+   Route::post('/api/posts', [ApiController::class, 'createPost']);
    Route::post('/api/posts/{post}/like', [ApiController::class, 'toggleLike']);
    Route::post('/api/posts/{post}/comments', [ApiController::class, 'postComment']);
    Route::get('/api/posts/{id}', [ApiController::class, 'getDetailPost']);
